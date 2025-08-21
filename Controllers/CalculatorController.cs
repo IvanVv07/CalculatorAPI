20aebb53ff567265654e9df1e8a95b8d1240ffee
Ivan <a2021116557@estudiantes.upsa.edu.bo>
2025-08-20 21:24:43 -0400

    Add subtract, multiply and divide endpoints

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -6,10 +6,31 @@ namespace CalculatorAPI.Controllers
     [Route("api/[controller]")]
     public class CalculatorController : ControllerBase
     {
+        
         [HttpGet("sum")]
         public ActionResult<double> Sum([FromQuery] double a, [FromQuery] double b)
         {
             return Ok(a + b);
         }
+        [HttpGet("subtract")]
+        public ActionResult<double> Subtract([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(a - b);
+        }
+
+        [HttpGet("multiply")]
+        public ActionResult<double> Multiply([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(a * b);
+        }
+
+        [HttpGet("divide")]
+        public ActionResult<double> Divide([FromQuery] double a, [FromQuery] double b)
+        {
+            if (b == 0)
+                return BadRequest("b no puede ser 0.");
+            return Ok(a / b);
+        }
     }
+    
 }
